bdb6b7eb3a2ad48750683a3852a28e7a8502c7b4
jolenecpn <jolene.cheongg@gmail.com>
2024-02-22 00:21:13 +0800

    skeleton of login and sign up

--- app/src/main/java/com/example/armadapp/LoginScreen.kt
+++ app/src/main/java/com/example/armadapp/LoginScreen.kt
@@ -2,7 +2,72 @@ package com.example.armadapp
 
 import androidx.compose.runtime.Composable
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 @Composable
-fun LoginScreen() {
-    //TODO: LoginScreen UI using composables and Database
+fun LoginScreen(
+    userName: MutableState<String> = mutableStateOf(""),
+    userID: MutableState<String> = mutableStateOf(""),
+    modifier: Modifier = Modifier,
+) {
+    var isButtonEnabled by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Team 20",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        OutlinedTextField(
+            value = userName.value,
+            onValueChange = { userName.value = it },
+            label = { Text("User Name") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+        )
+
+        OutlinedTextField(
+            value = userID.value,
+            onValueChange = { userID.value = it },
+            label = { Text("Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
+        )
+
+        Button(
+            onClick = {
+                //authenticate login and redirect to dashboard
+            },
+            enabled = userName.value.isNotEmpty()
+                    && userID.value.isNotEmpty(),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Login")
+        }
+
+        Button(
+            onClick = {
+                //route to sign up screen
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Sign Up")
+        }
+    }
 }
--- app/src/main/java/com/example/armadapp/SignUpScreen.kt
+++ app/src/main/java/com/example/armadapp/SignUpScreen.kt
@@ -2,7 +2,92 @@ package com.example.armadapp
 
 import androidx.compose.runtime.Composable
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
 @Composable
-fun SignUpScreen() {
+fun SignUpScreen(
+    name: MutableState<String> = mutableStateOf(""),
+    userName: MutableState<String> = mutableStateOf(""),
+    userEmail: MutableState<String> = mutableStateOf(""),
+    password: MutableState<String> = mutableStateOf(""),
+    modifier: Modifier = Modifier
+) {
     //TODO: SignUpScreen UI using composables and Database
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(8.dp)
+    )
+    {
+        Text(
+            text = "Sign Up",
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        TextField(
+            value = name.value,
+            onValueChange = { name.value = it },
+            label = { Text("Name") },
+            placeholder = { Text("Name") },
+            modifier = Modifier.padding(16.dp)
+        )
+
+        TextField(
+            value = userEmail.value,
+            onValueChange = { userEmail.value = it },
+            label = { Text("SIT Email") },
+            placeholder = { Text("SIT Email") },
+            modifier = Modifier.padding(16.dp)
+        )
+
+        TextField(
+            value = userName.value,
+            onValueChange = { userName.value = it },
+            label = { Text("User Name") },
+            placeholder = { Text("User Name") },
+            modifier = Modifier.padding(16.dp)
+        )
+
+        TextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            label = { Text("Password") },
+            placeholder = { Text("Password") },
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Button(
+            onClick = {
+                //backend code to access db
+            },
+            enabled = userName.value.isNotEmpty()
+                    && userEmail.value.isNotEmpty()
+                    && password.value.isNotEmpty(),
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text("Sign Up")
+        }
+
+    }
+
 }
